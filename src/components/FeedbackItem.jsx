a413b0b7cc768427ffdce368d9796d0e487a6b09
pkasss <paul@pauls-Air.home>
2022-08-31 19:51:21 +0200

    completed card component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,17 @@
+import Card from '../shared/Card'
+import PropTypes from 'prop-types'
 
-const FeedbackItem = ({item}) => {
-  
+const FeedbackItem = ({ item }) => {
   return (
-    <div className='card'>
+    <Card >
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.node.isRequired
 }
 
 export default FeedbackItem
